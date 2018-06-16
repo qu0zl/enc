4de010b5f510155f32df7a1b48cd32940ed3d68b
Greg Farrell <greg@gregfarrell.org>
2018-06-16 15:30:08 +0100

    Initial placeholders for Tsar generator

---
+++ tsar.js
@@ -0,0 +1,26 @@
+/* Copyright 2018 Greg Farrell <greg at gregfarrell dot org>
+   This code may not be used without permission.
+   If you'd like to use this for your own product then please ask and I'll swap you the rights to use it for that purpose in exchange for a signed copy of your product or some such. */
+
+var tsar = {
+    AcidRain:
+    {
+        name:"Acid Rain",
+        text:"blah blah blah",
+    },
+    ChaosBeast:
+    {
+        name:"Chaos Beast",
+        text:"blah blah blah",
+    },
+    RockFall:
+    {
+        name:"Rock Fall",
+        text:"blah blah blah",
+    },
+}
+
+var enc = {
+    "Chaos_Rift_Day":{name:"Chaos Rift (Day)", weighting_total:100, weighted_items:[[1,5,tsar.RockFall],[6,11,tsar.AcidRain]]},
+
+};
